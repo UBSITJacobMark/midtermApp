50311b1797f3b8ad07274efe27161d967c9e1f71
UBSITJacobMark <20244156@s.ubaguio.edu>
2026-02-27 13:59:42 +0800

    Interpolation

--- MidtermsApp/src/databinding/interpolation/interpolation.ts
+++ MidtermsApp/src/databinding/interpolation/interpolation.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { JsonPipe } from '../../../node_modules/@angular/common/types/_common_module-chunk';
+import { FormsModule } from '../../../node_modules/@angular/forms/types/forms';
 
 @Component({
   selector: 'app-interpolation',
-  imports: [],
+  imports: [JsonPipe, FormsModule],
   templateUrl: './interpolation.html',
   styleUrl: './interpolation.css',
 })
-export class Interpolation {}
+export class Interpolation {
+  id?: number;
+  name?: string | undefined;
+  position?: string;
+  department?: string;
+  details?: string;
+  role?: string | undefined;
+
+  title: string = 'Angular Data Binding Demo';
+  studentName: string = 'Maria Santos';
+  courseCode: string = 'APPDEV1';
+  currentYear: number = new Date().getFullYear();
+  roles = ['Admin', 'Editor', 'Viewer'];
+  text: string = "{(expression})"
+  curly: string ="1{ }}"
+  common: string ="{( userName ))"
+  methods: string= "{{ getStatus() }"
+  dotnot: string = "{{ user.name }}"
+  dotnot2: string = "{{ user?.profile?.email }}"
+  jpipe: string ="({ user | json ))"
+  isActive: boolean = false;
+}
+
---
+++ MidtermsApp/src/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    name?: string | undefined;
+    position?: string;
+    department?: string;
+        details?: string;
+            role?: string | undefined;
+}
